a9346ba78de790281966ce8c88bbd52eeaf0ca94
Arpit Kumar Chauhan <kumarchauhanarpit@gmail.com>
2025-09-26 15:03:40 +0530

    Basic Design and Backend setup

---
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import LeftSection from './LeftSection';
+import RightSection from './RightSection';
+import UniverseSection from './UniverseSection';
+import Footer from './Footer';
+function ProductsPage() {
+    return (  
+        <>
+        <Navbar/>
+        <Hero/>
+        <LeftSection/>
+        <RightSection/>
+        <UniverseSection/>
+        <Footer/>
+        </>
+    );
+}
+
+export default ProductsPage;
---
+++ frontend/src/landing_page/signup/Singup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NabvBar from '../NabvBar';
+import Footer from '../Footer';
+function Singup() {
+    return (  
+        <>
+            <NabvBar />
+            <h1>Signup!</h1>
+            <Footer/>
+        </>
+    );
+}
+
+export default Singup;
